115bee0b273825397ed5db21103533ecfec056d9
Mgoumane <goumane.mahmoudou@gmail.com>
2021-02-10 16:43:00 +0100

    je passe la main à admam - route créer

--- moovice/src/App.js
+++ moovice/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+
+
+      <BrowserRouter>
+        <ul>
+          <li> <Link to="/"><h1>Home</h1></Link></li>
+          <li><Link to="/weekly"><h1>Weekly</h1></Link></li>
+          <li><Link to="/weekly-battle"><h1>WeklyBattle</h1></Link></li>
+          <li><Link to="/popular"><h1>Popular</h1></Link></li>
+          <li><Link to="/popular-battle "><h1>PopularBattle</h1></Link></li>
+          <li><Link to="/favorites"><h1>Favorites</h1></Link></li>
+        </ul>
+        <Switch>
+          <Route path="/" component={App} />
+          <Route path="/weekly" component={App} />
+          <Route path="/weekly-battle" component={App} />
+          <Route path="/popular" component={App} />
+          <Route path="/popular-battle " component={App} />
+          <Route path="/favorites" component={App} />
+        </Switch>
+      </BrowserRouter>
+
+    );
+  }
+
 }
 
 export default App;
