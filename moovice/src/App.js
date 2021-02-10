d6e92d6e19751021df645fb4e0dba0f8377da9a0
unknown <samassaadama@hotmail.fr>
2021-02-10 17:16:53 +0100

    Adama passe la main à Mams

--- moovice/src/App.js
+++ moovice/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom"
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import './App.css';
+import Popular from './components/Popular';
 
 class App extends React.Component {
 
@@ -9,22 +10,27 @@ class App extends React.Component {
 
 
       <BrowserRouter>
-        <ul>
-          <li> <Link to="/"><h1>Home</h1></Link></li>
-          <li><Link to="/weekly"><h1>Weekly</h1></Link></li>
-          <li><Link to="/weekly-battle"><h1>WeklyBattle</h1></Link></li>
-          <li><Link to="/popular"><h1>Popular</h1></Link></li>
-          <li><Link to="/popular-battle "><h1>PopularBattle</h1></Link></li>
-          <li><Link to="/favorites"><h1>Favorites</h1></Link></li>
-        </ul>
-        <Switch>
-          <Route path="/" component={App} />
-          <Route path="/weekly" component={App} />
-          <Route path="/weekly-battle" component={App} />
-          <Route path="/popular" component={App} />
-          <Route path="/popular-battle " component={App} />
-          <Route path="/favorites" component={App} />
-        </Switch>
+        <div>
+          <nav>
+            <ul>
+              <li> <Link to="/"><h1>Home</h1></Link></li>
+              <li><Link to="/weekly"><h1>Weekly</h1></Link></li>
+              <li><Link to="/weekly-battle"><h1>WeklyBattle</h1></Link></li>
+              <li><Link to="/popular"><h1>Popular</h1></Link></li>
+              <li><Link to="/popular-battle "><h1>PopularBattle</h1></Link></li>
+              <li><Link to="/favorites"><h1>Favorites</h1></Link></li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/" component={App} />
+            <Route path="/weekly" component={App} />
+            <Route path="/weekly-battle" component={App} />
+            <Route path="/popular" component={App} />
+            <Route path="/popular-battle " component={App} />
+            <Route path="/favorites" component={App} />
+          </Switch>
+        </div>
       </BrowserRouter>
 
     );
---
+++ moovice/src/components/Popular.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Popular extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            movies: [''],
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+}
+     
+export default Popular
