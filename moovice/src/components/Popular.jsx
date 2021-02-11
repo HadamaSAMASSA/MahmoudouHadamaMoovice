994e4d90fb25d95700e1716333bccb76538214a5
unknown <samassaadama@hotmail.fr>
2021-02-11 11:41:33 +0100

    récupération de la liste

--- moovice/src/components/Popular.jsx
+++ moovice/src/components/Popular.jsx
@@ -4,8 +4,21 @@ class Popular extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            movies: [],
+            movies: [
+                
+            ],
         }
+    }
+
+    getMoovies(movie){
+        fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key={20f40c1217f9ae14d7e03e4b99bca61b}")
+        .then((reponse) => reponse.json())
+        .then((apiReponse) => {
+            this.setState ({
+                movies: apiReponse
+            });
+        }
+        )  
     }
 
     render(){
